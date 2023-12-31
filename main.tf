74def9976c8efe6c62e48c5cfecc566208cda7b6
veraborvinski <72395680+veraborvinski@users.noreply.github.com>
2023-12-31 14:31:38 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,5 +3,5 @@ resource "google_compute_instance_from_machine_image" "tpl" {
   name     = var.machine_name
   zone     = var.zone
 
-  source_machine_image = "projects/${var.project}/global/machineImages/videoserver"
+  source_machine_image = "projects/${var.project}/global/machineImages/catalogueserver"
 }
